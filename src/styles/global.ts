8032bed52b796f4a6088911c94a8e71283c0fc3c
erickmarquesc <erick.marquesc@hotmail.com>
2022-10-07 12:06:46 -0300

    refactor: :art: format of the code

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -18,7 +18,7 @@ export const GlobalStyle = createGlobalStyle`
     -webkit-font-smoothing: antialiased;
   };
 
-  border-style, input-security, textarea, button{
+  body, input-security, textarea, button{
     font: 400 1rem Roboto, sans-serif;
   }
 `;
