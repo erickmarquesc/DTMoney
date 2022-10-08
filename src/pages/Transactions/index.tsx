7bbef1f5ca060d117e4c3e9b7dbe298a94aea597
erickmarquesc <erick.marquesc@hotmail.com>
2022-10-07 23:39:12 -0300

    feat: :zap: created transactions table

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,38 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { PriceHighLight, TransactionsContainer, TransactionsTable } from "./styles";
 
 export function Transactions() {
   return (
     <div>
+
       <Header />
-      <Summary/>
+      <Summary />
+
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tr>
+            <td width="50%">Desenvolvivmento de site</td>
+            <td>
+              <PriceHighLight variant="income">
+                R$ 12.000,00
+              </PriceHighLight>
+            </td>
+            <td>Venda</td>
+            <td>13/04/2022</td>
+          </tr>
+          <tr>
+            <td width="50%">Burgão</td>
+            <td>
+              <PriceHighLight variant="outcome">
+                - R$ 12,00
+              </PriceHighLight>
+            </td>
+            <td>Alimentação</td>
+            <td>10/04/2022</td>
+          </tr>
+        </TransactionsTable>
+      </TransactionsContainer>
     </div>
   );
 };
