7b109851348f8374188e5faed13f4b2d72bf425d
erickmarquesc <erick.marquesc@hotmail.com>
2022-10-08 01:51:18 -0300

    feat: :zap: created searchForm component

---
+++ src/components/SearchForm/styles.ts
@@ -0,0 +1,40 @@
+import styled from "styled-components";
+
+export const SearchFormContainer = styled.form`
+  gap: 1rem;
+  display: flex;
+
+  input{
+    flex: 1;
+    border: 0;
+    padding: 1rem;
+    border-radius: 6px;
+    color: ${(props)=>props.theme["gray-500"]};
+    background: ${(props)=>props.theme["gray-900"]};
+
+    &::placeholder{
+      color: ${(props)=>props.theme["gray-500"]};
+    }
+  }
+
+  button{
+    border: 0;
+    gap: 0.75rem;
+    padding: 1rem;
+    display: flex;
+    font-weight: bold;
+    border-radius: 6px;
+    align-items: center;
+    background: transparent;
+    color: ${(props)=>props.theme["green-300"]};
+    border: 1px solid ${(props)=>props.theme["green-300"]};
+
+    transition: border-color 0.2s, color 0.2s, background-color 0.2s;
+
+    &:hover{
+      color: ${(props)=>props.theme.white};
+      background: ${(props)=>props.theme["green-500"]};
+      border-color: ${(props)=>props.theme["green-500"]};
+    }
+  }
+`;
