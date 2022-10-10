84fdd4682b58163cf05df7c19683be44e04b4b7b
erickmarquesc <erick.marquesc@hotmail.com>
2022-10-10 14:06:04 -0300

    feat: :zap: created hook

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,7 +1,15 @@
-import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from "phosphor-react";
+import { ArrowCircleDown, ArrowCircleUp, ArrowCounterClockwise, CurrencyDollar } from "phosphor-react";
+import { useContext } from "react";
+import { TransactionsContext } from "../../contexts/TransactionsContext";
+import { useSummary } from "../../hooks/useSummary";
+import { priceFormatter } from "../../utils/formatter";
 import { SummaryCard, SummaryContainer } from "./styled";
 
 export function Summary() {
+
+  const {income,outcome,total} = useSummary();
+
+
   return (
     <SummaryContainer>
       <SummaryCard type='CircleUp'>
@@ -11,7 +19,7 @@ export function Summary() {
           <ArrowCircleUp size={32} />
         </header>
 
-        <strong>R$ 17.400,00</strong>
+        <strong>{priceFormatter.format(income)}</strong>
 
       </SummaryCard>
 
@@ -22,7 +30,7 @@ export function Summary() {
           <ArrowCircleDown size={32} />
         </header>
 
-        <strong>R$ 17.400,00</strong>
+        <strong>{priceFormatter.format(outcome)}</strong>
 
       </SummaryCard>
 
@@ -33,7 +41,7 @@ export function Summary() {
           <CurrencyDollar size={32} />
         </header>
 
-        <strong>R$ 17.400,00</strong>
+        <strong>{priceFormatter.format(total)}</strong>
 
       </SummaryCard>
 
