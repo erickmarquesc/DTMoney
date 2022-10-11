a8998c5f638370c702acb6c148e1a31f6c70542a
erickmarquesc <erick.marquesc@hotmail.com>
2022-10-10 22:35:22 -0300

    feat: :sparkles: using context

--- src/components/SearchForm/index.tsx
+++ src/components/SearchForm/index.tsx
@@ -1,7 +1,9 @@
+import { TransactionsContext } from "../../contexts/TransactionsContext";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { MagnifyingGlass } from "phosphor-react";
 import { SearchFormContainer } from "./styles";
 import { useForm } from "react-hook-form";
+import { useContext } from "react";
 import * as z from 'zod';
 
 const searchFormSchema = z.object({
@@ -11,13 +13,15 @@ const searchFormSchema = z.object({
 type SearchFormInput = z.infer<typeof searchFormSchema>;
 
 export function SearchForm() {
+  
+  const {fetchTransactions} = useContext(TransactionsContext);
 
   const { register, handleSubmit, formState: { isSubmitting } } = useForm<SearchFormInput>({
     resolver: zodResolver(searchFormSchema),
   });
 
-  function handleSearchTransactions(data: SearchFormInput) {
-
+  async function handleSearchTransactions(data: SearchFormInput) {
+    await fetchTransactions(data.query);
   };
 
   return (
